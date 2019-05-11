1b378271cd64288683b89c61114f7328059a050e
jussimar <jussimar@jussimarleal.com.br>
2019-05-11 13:03:51 -0300

    comandos com sensores e escrita na tela de led -  final da aula

--- comandoPlca.py
+++ comandoPlca.py
@@ -6,11 +6,22 @@ sense = SenseHat()
 #criando variaveis com cores
 red = (255,0,0)
 green = (0,255,0)
+blue = (0,0,255)
 #controla a velodidade a padrão é 0.1
 scroll_speed = 0.005
 
+sense.show_message("Olá Mundo!",scroll_speed,green)
+nome = input("Digite seu nome: ")
+sense.show_message(nome,scroll_speed,green)
+#limpando matriz de led
+sense.clear()
+
+int_red = 0
+int_green = 0
+int_azul = 0
+cor = (int_red, int_green, int_azul)
+
 while True:
-    sense.show_message("Olá Mundo!",scroll_speed,green)
     time.sleep(1)
     sense.clear(red)
     time.sleep(5)
@@ -24,3 +35,22 @@ while True:
     temperature = sense.get_temperature()
     print("Temperatura: " + str(temperature))
     sense.show_message("Temperatura: " + str(temperature),scroll_speed,green)
+    
+    #Como iremos controlar a matriz de leds
+    #feedback de temperatura
+    #Se a temperatura for de 0 a 30 gradus, gradativamenteaumenta o tom de azul
+    int_red = 0
+    int_green = 0
+    int_azul = 0
+    if (temperature > 0 and temperature <= 30):
+          int_azul = (temperature * 8.6) #numa escla de zero a 255
+          int_green = 0
+          int_red = 0;
+    elif (temperature > 30 and temperature < 101):
+          int_azul = 0
+          int_green = 0
+          int_red = (temperature*3.64)
+   
+    
+    sense.clear(cor)
+        
