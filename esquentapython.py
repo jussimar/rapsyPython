c151f0a23534cba1d98ed1f67855852af2dbe18a
jussimar <jussimar@jussimarleal.com.br>
2019-05-11 12:08:51 -0300

    mais comandos for if while

--- esquentapython.py
+++ esquentapython.py
@@ -1,9 +1,26 @@
+import time
+
 print("Olá Mundo")
-variavelUsuario = input("Digite alguma coisa")
+variavelUsuario = input("Digite alguma coisa: ")
 
 variavelFloat = float(variavelUsuario);
 
 if variavelFloat == 3.14:
     print("Dia do RaspBerry Pi")
+elif variavelFloat == 42.0:
+    print("Obrigado pelos peixes")
 else:
-    print("vem o proximo")
+    print("até mais")
+    
+
+for x in range(100):
+    print("Nunca mais vou aprontar " + str(x+1))
+    
+tempo = 0
+while tempo < 10:
+    print("Tempo passando..." + str(tempo))
+    #espera um segundo o fluxo do programa
+    time.sleep(1)
+    tempo = tempo + 1
+    
+print("O programa acabou")
