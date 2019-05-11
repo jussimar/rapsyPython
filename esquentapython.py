2aa1869c330f00d8e942869b590234ef1237f761
jussimar <jussimar@jussimarleal.com.br>
2019-05-11 11:54:29 -0300

    Introdução ao python

---
+++ esquentapython.py
@@ -0,0 +1,9 @@
+print("Olá Mundo")
+variavelUsuario = input("Digite alguma coisa")
+
+variavelFloat = float(variavelUsuario);
+
+if variavelFloat == 3.14:
+    print("Dia do RaspBerry Pi")
+else:
+    print("vem o proximo")
